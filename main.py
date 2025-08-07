14c26680923a33191c12ebe517ea103e7bbd6eb8
Vazoubella <osvaldo9margareth@accountsacessos.com>
2025-08-07 10:52:55 -0300

    ...

--- main.py
+++ main.py
@@ -25,5 +25,4 @@ def send_message(chat_id, text):
     requests.post(url, json=payload)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
